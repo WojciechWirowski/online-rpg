efa6b1c19c0938a6dccc84d5bece365539b53344
wojciu <wojciech.wirowski@outlook.com>
2024-01-20 00:17:14 +0100

    Changing the map feature v2

--- src/components/GameArena.js
+++ src/components/GameArena.js
@@ -5,18 +5,17 @@ const GameArena = ({selectedMap}) => {
     const renderMap = () => {
         switch (selectedMap) {
             case 'Map1':
-                return <div>Map1</div>
+                return <div className="map-content map1">Map1</div>
             case 'Map2':
-                return <div>Map2</div>
-            case 'Map2':
-                return <div>Map3</div>
+                return <div className="map-content map2">Map2</div>
+            case 'Map3':
+                return <div className="map-content map3">Map3</div>
         }
     }
 
     return (
         <div className="gameArena">
-            <h2>{selectedMap}</h2>
-            {renderMap}
+            {renderMap()}
         </div>
     )
 }
