8a421fa2a4423b40e127079182b01b9113e88b43
wojciu <wojciech.wirowski@outlook.com>
2024-01-21 23:28:42 +0100

    No shit done xd

--- src/components/GameArena.js
+++ src/components/GameArena.js
@@ -7,15 +7,15 @@ const GameArena = ({selectedMap}) => {
         switch (selectedMap) {
             case 'Map1':
                 return <div className="map-content map1">
-                    <Grid rows={10} columns={10} cellSize={50} placedObjects={[1, 5, 15]}/>
+                    <Grid rows={40} columns={40} cellSize={15} placedObjects={[1, 5, 15]}/>
                 </div>
             case 'Map2':
                 return <div className="map-content map2">
-                    <Grid rows={10} columns={10} cellSize={50} placedObjects={[5, 5, 15]}/>
+                    <Grid rows={40} columns={40} cellSize={50} placedObjects={[5, 5, 15]}/>
                 </div>
             case 'Map3':
                 return <div className="map-content map3">
-                    <Grid rows={10} columns={10} cellSize={50} placedObjects={[8, 2, 15]}/>
+                    <Grid rows={40} columns={40} cellSize={50} placedObjects={[8, 2, 15]}/>
                 </div>
             default:
                 return null;
