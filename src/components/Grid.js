757234749e451f970e1239a90836ff0a06743c62
wojciu <wojciech.wirowski@outlook.com>
2024-01-22 18:05:55 +0100

    Grid's cells fixed to fill the whole user screen no matter how bit it is

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,9 +1,34 @@
 // Grid.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
-const Grid = ({ rows, columns, cellSize, placedObjects }) => {
+const Grid = ({ rows, columns, placedObjects }) => {
+  const [cellSize, setCellSize] = useState(0);
+
+  useEffect(() => {
+    // Calculate the cell size to fill the screen height
+    const screenHeight = window.innerHeight;
+    const calculatedCellSize = Math.floor(screenHeight / rows);
+
+    // Update the state with the calculated cell size
+    setCellSize(calculatedCellSize);
+
+    // Event listener for window resize to recalculate cell size
+    const handleResize = () => {
+      const newScreenHeight = window.innerHeight;
+      const newCellSize = Math.floor(newScreenHeight / rows);
+      setCellSize(newCellSize);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [rows]);
+
   const gridStyle = {
     gridTemplateRows: `repeat(${rows}, ${cellSize}px)`,
     gridTemplateColumns: `repeat(${columns}, ${cellSize}px)`,
